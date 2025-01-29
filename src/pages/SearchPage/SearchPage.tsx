c9af636b9a46dc2c9b8006d4cfb1005a64c31ea9
just-doks <graviman13@gmail.com>
2025-01-29 22:31:21 +0300

    just-doks [feat]: Added route and type of SearchPage component

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,8 +1,9 @@
+import { FC } from "react";
 import './SearchPage.css'
 import {SearchBar} from "src/components/SearchBar";
 import { CharacterSlider } from '../../components/CharacterList'
 
-export const SearchPage = () => {
+export const SearchPage: FC = () => {
     return (
     <div className='container searchpage-wrapper'>
         <h1 className='searchpage-title'>Search for your favorite characters right here!</h1>
