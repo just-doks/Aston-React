5103f720533e07c1485677526d4658c03638e814
Vladimir Lyamkov <vladimir-lyamkov@yandex.by>
2025-01-30 21:19:47 +0300

    HandleWith [fix]: fix conflicts

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,3 +1,17 @@
-import './SearchBar.css'
+import { useState } from "react";
+import "./SearchBar.css";
+import { FilterMenu } from "./FilterMenu";
 
-export const SearchBar = () => <input type="text" className="searchbar" placeholder="Search..."/>
+export const SearchBar = () => {
+    const [dropdownVisible, setDropdownVisible] = useState(false)
+    const dropdownToggle = () => {
+        setDropdownVisible(!dropdownVisible)
+    }
+  return (
+  <div className="searchbar">
+    <button className="searchbar_filter" onClick={dropdownToggle}>Filter</button>
+    <input type="text" className="searchbar_input" placeholder="Search..." />
+    {dropdownVisible ? <FilterMenu/> : <></>}
+  </div>
+  )
+};
