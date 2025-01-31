c427f518101a00d24c191f6e1fea71b82436a550
Vladimir Lyamkov <vladimir-lyamkov@yandex.by>
2025-01-31 19:46:41 +0300

    HandleWith [refactor]: merge redux branch

---
+++ src/utils/validationSchemes.ts
@@ -0,0 +1,20 @@
+import * as Yup from "yup";
+
+export const authValidationSchema = Yup.object({
+  username: Yup.string()
+    .min(3, "Must be at least 3 characters")
+    .max(15, "Maximum 15 characters")
+    .required("Username required"),
+  password: Yup.string()
+    .min(6, "Must be at least 6 characters")
+    .max(15, "Maximum 15 characters")
+    .required("Password required"),
+});
+
+export const signInValidationSchema = Yup.object({
+  username: Yup.string(),
+  password: Yup.string()
+    .min(6, "Must be at least 6 characters")
+    .max(15, "Maximum 15 characters")
+    .required("Password required"),
+});
