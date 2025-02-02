4772fd2d92992750d07af752f3e893b5bda86298
just-doks <graviman13@gmail.com>
2025-02-02 20:02:35 +0300

    just-doks [feat]: Made Character page

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,20 @@
-import { FC } from "react";
+import { JSX } from "react";
 import { HomePage } from "./pages/HomePage";
 import { PATHS } from "./utils/constants";
+import { SearchPage } from "./pages/SearchPage/SearchPage";
+import { CharacterPage } from "./pages/CharacterPage";
 
-export const publicRoutes: {path: string, Component: FC}[] = [
+export const publicRoutes: {path: string, Component: (props: any) => JSX.Element}[] = [
     {
         path: PATHS.HOME,
         Component: HomePage
+    },
+    {
+        path: PATHS.SEARCH,
+        Component: SearchPage
+    },
+    {
+        path: PATHS.CHARACTER,
+        Component: CharacterPage
     }
 ]
