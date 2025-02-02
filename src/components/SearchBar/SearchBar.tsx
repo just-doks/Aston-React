1a95b3768b343bffb780c47b1160c8df3c2f18b8
Run4gate <westernisator@gmail.com>
2025-02-03 01:44:56 +0300

    Run4gate [feat]: add transition from home page

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -2,46 +2,62 @@ import { useState } from "react";
 import "./SearchBar.css";
 import { FilterMenu } from "./FilterMenu";
 import { Formik, Form, Field } from "formik";
-import { configureSearch, setSearchResults, setSearchError } from "../../store/searchSlice";
-import { useDispatch } from "react-redux";
+import {
+  configureSearch,
+  setSearchResults,
+  setSearchError,
+} from "../../store/searchSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { searchConfig } from "../../utils/selectors";
 import { TypeFilters } from "../../http/characterTypes";
 import { fetchFilteredCharacters } from "../../http/characterAPI";
+import { useNavigate } from "react-router";
+import { PATHS } from "../../utils/constants";
 
 export const SearchBar: React.FC<{ filterPosition: string }> = (props) => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const initialConfig = useSelector(searchConfig);
   const [dropdownVisible, setDropdownVisible] = useState(false);
-  const initialValues:  TypeFilters = {
-    name: "",
-  }
-  const handleSubmit = (values:  TypeFilters) => {
-    dispatch(configureSearch(values))
-    fetchFilteredCharacters(values).then((data) => dispatch(setSearchResults(data))).catch((error)=> {dispatch(setSearchError(error))})
+  const handleSubmit = (values: TypeFilters) => {
+    dispatch(configureSearch(values));
+    fetchFilteredCharacters(values)
+      .then((data) => dispatch(setSearchResults(data)))
+      .catch((error) => {
+        dispatch(setSearchError(error));
+      })
+      .finally(() => navigate(PATHS.SEARCH));
   };
+
   const dropdownToggle = () => {
     setDropdownVisible(!dropdownVisible);
   };
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-     <Form className="searchbar">
-      <button
-        className="searchbar_filter"
-        type="button"
-        onClick={dropdownToggle}
-      >
-        Filter
-      </button>
-      {dropdownVisible ? <FilterMenu position={props.filterPosition} /> : <></>}
-      <Field
-        type="text"
-        name="name"
-        className="searchbar_input"
-        placeholder="Type here..."
-      />
+    <Formik initialValues={initialConfig} onSubmit={handleSubmit}>
+      <Form className="searchbar">
+        <button
+          className="searchbar_filter"
+          type="button"
+          onClick={dropdownToggle}
+        >
+          Filter
+        </button>
+        {dropdownVisible ? (
+          <FilterMenu position={props.filterPosition} />
+        ) : (
+          <></>
+        )}
+        <Field
+          type="text"
+          name="name"
+          className="searchbar_input"
+          placeholder="Type here..."
+        />
 
-      <button className="searchbar_submit" type="submit">
-        Search
-      </button>
-     </Form>
+        <button className="searchbar_submit" type="submit">
+          Search
+        </button>
+      </Form>
     </Formik>
   );
 };
