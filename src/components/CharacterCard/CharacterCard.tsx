a8327b6a341640c40aa2caaca53f6f6aa6dc55a1
just-doks <graviman13@gmail.com>
2025-02-03 15:50:35 +0300

    just-doks [fix]: added handler and small changes

--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -1,4 +1,4 @@
-import { useState, JSX } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router";
 import './CharacterCard.css';
 import type { CharacterSchema } from "src/http/characterTypes";
@@ -6,18 +6,22 @@ import { PATHS } from "src/utils/constants";
 import { StarButton } from "./StarButton";
 import { PlanetSpinner } from "./PlanetSpinner";
 
-export function CharacterCard({character = undefined}: { character: CharacterSchema }
-): JSX.Element {
+export function CharacterCard({character}: 
+    { character: CharacterSchema }
+) {
     const [checked, setChecked] = useState<boolean>(false);
     const navigate = useNavigate();
     function handleLabelClick() {
         navigate(PATHS.CHARACTER, { state: {character: character}})
     }
+    function handleStarChange() {
+        setChecked(!checked)
+    }
 
     return(
         <div className="c-card-container">
-        { character === undefined 
-            ? <PlanetSpinner className=" c-card-spinner"/>
+        { !character 
+            ? <PlanetSpinner className="c-card-spinner"/>
             : <>
                 <img className="c-card-image" src={character.image} alt="image"/>
                  <label className="c-card-label" onClick={handleLabelClick}>
@@ -26,7 +30,7 @@ export function CharacterCard({character = undefined}: { character: CharacterSch
                 <StarButton 
                     className="c-card-fav" 
                     checked={checked} 
-                    onChange={() => setChecked(!checked)}
+                    onChange={handleStarChange}
                 />
               </>  
         }
