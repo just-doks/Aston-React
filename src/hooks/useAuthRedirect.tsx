ba7b395f2367f057b88ab08f8e1c434799cac4b3
Vladimir Lyamkov <vladimir-lyamkov@yandex.by>
2025-02-05 13:19:24 +0300

    HandleWith [fix]: fix change state during render component authForm

--- src/hooks/useAuthRedirect.tsx
+++ src/hooks/useAuthRedirect.tsx
@@ -1,10 +1,15 @@
+import { useEffect } from "react";
 import { PATHS } from "../utils/constants";
 import { isAuth as auth } from "../utils/selectors";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router";
 
 export const useAuthRedirect = () => {
-    const isAuth = useSelector(auth);
-    const navigate = useNavigate()
-    isAuth && navigate(PATHS.HOME)
-}
+  const isAuth = useSelector(auth);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isAuth) {
+      navigate(PATHS.HOME);
+    }
+  }, [isAuth, navigate]);
+};
