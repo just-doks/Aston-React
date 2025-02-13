2a8ab5f3e20dbbb62a1404d7ce10faf87ede3b6f
just-doks <graviman13@gmail.com>
2025-02-13 22:40:17 +0300

    just-doks [fix]: added fix to middleware

--- src/middlewares/favoriteMiddlewares.ts
+++ src/middlewares/favoriteMiddlewares.ts
@@ -1,7 +1,7 @@
 import type { Middleware, ThunkMiddleware, UnknownAction } from "@reduxjs/toolkit";
 import type { RootState } from "src/store/store.ts";
 import { removeFavoritesFromQueue, initFavorites } from "src/store/favoriteSlice";
-import { loginUser } from "src/store/authSlice";
+import { loginUser, registerUser } from "src/store/authSlice";
 
 export const favMiddleware: Middleware<{}, RootState> = api => next => action => {
     if (removeFavoritesFromQueue.match(action)) {
@@ -15,7 +15,7 @@ export const favMiddleware: Middleware<{}, RootState> = api => next => action =>
 
 export const favThunk: ThunkMiddleware<RootState, UnknownAction, unknown> = 
 ({ dispatch, getState }) => next => action => {
-    if (loginUser.match(action)) {
+    if (loginUser.match(action) || registerUser.match(action)) {
         next(action);
         const state = getState();
         dispatch(initFavorites(state.auth.loginUser.username));
