2d7dadd01b636bc4ffb626087de7fd9b1593dd03
Run4gate <westernisator@gmail.com>
2025-02-14 21:24:44 +0300

    Run4gate [fix]: strictMode re-render bug

--- src/hooks/useClearOnUnmount.tsx
+++ src/hooks/useClearOnUnmount.tsx
@@ -1,14 +1,22 @@
 import { useRef, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { clearSearchConfig } from "../store/searchSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { clearSearchConfig, setSearchError } from "../store/searchSlice";
+import { searchError } from "../utils/selectors";
 
 export const useClearOnUnmount = () => {
   const firstRender = useRef(true);
+  const thereWasAnError = useRef('');
   const dispatch = useDispatch();
+  const error = useSelector(searchError);
   useEffect(() => {
     if (firstRender.current) {
+      thereWasAnError.current = error;
       firstRender.current = false;
       return;
+    }
+    if (error) {
+      dispatch(setSearchError(error))
+      thereWasAnError.current = '';
     }
 
     return () => {
